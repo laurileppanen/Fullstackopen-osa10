135880be32607972fee31a2a934d4daabc3a6ada
Lauri Leppänen <late-22-@hotmail.com>
2024-03-18 15:12:21 +0200

    10.3

---
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+
+const Item = ({
+  fullName,
+  description,
+  language,
+  stars,
+  forks,
+  reviews,
+  rating,
+}) => (
+  <View>
+    <Text>Full name: {fullName}</Text>
+    <Text>Description: {description}</Text>
+    <Text>Language: {language}</Text>
+    <Text>Stars: {stars}</Text>
+    <Text>Forks: {forks}</Text>
+    <Text>Reviews: {reviews}</Text>
+    <Text>Rating: {rating}</Text>
+  </View>
+);
+
+export default Item;
