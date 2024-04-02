1c64628f8beefea0a2e86036111107678595ee3b
Lauri Leppänen <late-22-@hotmail.com>
2024-04-02 12:25:07 +0300

    10.6

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -2,12 +2,14 @@ import { View, StyleSheet, Pressable } from "react-native";
 import Constants from "expo-constants";
 import Text from "./Text";
 import theme from "../theme";
+import { Link } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight + 30,
     backgroundColor: theme.colors.appBarBackgound,
     paddingVertical: 30,
+    flexDirection: "row",
   },
   textStyle: {
     fontSize: theme.fontSizes.appBar,
@@ -20,7 +22,14 @@ const AppBar = () => {
   return (
     <View style={styles.container}>
       <Pressable>
-        <Text style={styles.textStyle}>Repositories</Text>
+        <Link to="/">
+          <Text style={styles.textStyle}>Repositories</Text>
+        </Link>
+      </Pressable>
+      <Pressable>
+        <Link to="/signin">
+          <Text style={styles.textStyle}>Sign in</Text>
+        </Link>
       </Pressable>
     </View>
   );
